286466fafd8b39643109bfd06983cf7cf53dc712
MatthewBert <47572136+MatthewBert@users.noreply.github.com>
2024-04-30 15:38:05 -0400

    Added logic to handle resizing based on user input and clearing the existing grid

--- script.js
+++ script.js
@@ -3,27 +3,71 @@ const container = document.querySelector("#container");
 const containerWidth = container.offsetWidth;
 const containerHeight = container.offsetHeight;
 
-buildGrid();
+const resizeButton = document.querySelector("#resize");
+const clearButton = document.querySelector("#clear");
 
-function buildGrid(){
-    for(let row = 0; row < 16; row++){
-        for(let col = 0; col < 16; col++){
+//builds the initial grid (16x16px)
+let gridSize = 16;
+buildGrid(gridSize);
+
+//event listener for resizing the grid
+resizeButton.addEventListener("click", function (e) {
+    var valid = 0;
+    gridSize = Number(prompt("Enter a number for the grid width and height:"));
+    while(valid == 0){
+        if(gridSize > 100){
+            gridSize = Number(prompt("Value cannot exceed 100, please enter a number for the grid width and height:"));
+        } else if(isNaN(gridSize)){
+            gridSize = Number(prompt("Value is not a number, please enter a number for the grid width and height:"));
+        } else {
+            valid = 1;
+        }
+    }
+    deleteGrid();
+    buildGrid(gridSize);
+});
+
+//event listener for reseting the grid
+clearButton.addEventListener("click", function(e){
+    clearGrid(gridSize);
+});
+
+
+//Generates a new grid based on size
+//Size: user input cannot be greater than 100
+function buildGrid(size) {
+    for (let row = 0; row < size; row++) {
+        for (let col = 0; col < size; col++) {
             const grid = document.createElement("div");
-    
+
             //set the grid size equal to the size of the container
-            const gridWidth = containerWidth/16;
+            const gridWidth = containerWidth / size;
             grid.style.width = `${gridWidth}px`;
-            const gridHeight = containerHeight/16;
+            const gridHeight = containerHeight / size;
             grid.style.height = `${gridHeight}px`;
-    
+
             grid.classList.add("grid");
-    
-            grid.addEventListener("mouseover", colorBlack)
+
+            grid.addEventListener("mouseover", colorBlack);
             container.appendChild(grid);
         }
     }
 }
 
-function colorBlack(e){
+//deletes the existing grid
+function deleteGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+//clear the grid but keeps the size
+function clearGrid(size){
+    console.log("clearing grid");
+    deleteGrid();
+    buildGrid(size);
+}
+
+function colorBlack(e) {
     e.target.style.backgroundColor = "black";
 }
